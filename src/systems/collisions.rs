94fa0ac43d3df7940462be3e6e32d42b8903c792
Trevor Makes <the.trevor.makes@gmail.com>
2022-06-25 23:57:56 -0700

    Add enemy spawner and collision system

---
+++ src/systems/collisions.rs
@@ -0,0 +1,18 @@
+use crate::prelude::*;
+
+#[system]
+#[read_component(Point)]
+#[read_component(Player)]
+#[read_component(Enemy)]
+pub fn collisions(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
+    let mut player_pos = Point::zero();
+    <&Point>::query()
+        .filter(component::<Player>())
+        .iter(ecs)
+        .for_each(|pos| player_pos = *pos);
+    <(Entity, &Point)>::query()
+        .filter(component::<Enemy>())
+        .iter(ecs)
+        .filter(|(_, pos)| **pos == player_pos)
+        .for_each(|(entity, _)| commands.remove(*entity));
+}
