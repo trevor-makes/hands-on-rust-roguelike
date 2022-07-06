8b6f90e76a50639e77d50aa778de07acad7aaf7c
Trevor Makes <the.trevor.makes@gmail.com>
2022-07-06 02:12:37 -0700

    Add game over state

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -2,8 +2,15 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(WantsToAttack)]
+#[read_component(Player)]
 #[write_component(Health)]
 pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
+    let player = <Entity>::query()
+        .filter(component::<Player>())
+        .iter(ecs)
+        .copied()
+        .next()
+        .unwrap();
     let victims = <(Entity, &WantsToAttack)>::query()
         .iter(ecs)
         .map(|(entity, attack)| (*entity, attack.victim))
@@ -11,7 +18,7 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     victims.iter().for_each(|(message, victim)| {
         if let Ok(mut health) = ecs.entry_mut(*victim).unwrap().get_component_mut::<Health>() {
             health.current -= 1;
-            if health.current < 1 {
+            if health.current < 1 && *victim != player {
                 commands.remove(*victim);
             }
         }
--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -1,11 +1,17 @@
 use crate::prelude::*;
 
 #[system]
-pub fn end_turn(#[resource] turn_state: &mut TurnState) {
-    let new_state = match turn_state {
-        TurnState::AwaitingInput => return,
-        TurnState::PlayerTurn => TurnState::MonsterTurn,
-        TurnState::MonsterTurn => TurnState::AwaitingInput,
+#[read_component(Health)]
+#[read_component(Player)]
+pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
+    let is_player_dead = <&Health>::query()
+        .filter(component::<Player>())
+        .iter(ecs)
+        .any(|hp| hp.current < 1);
+    *turn_state = match (is_player_dead, *turn_state) {
+        (true, _) => TurnState::GameOver,
+        (_, TurnState::PlayerTurn) => TurnState::MonsterTurn,
+        (_, TurnState::MonsterTurn) => TurnState::AwaitingInput,
+        (_, _) => return,
     };
-    *turn_state = new_state;
 }
