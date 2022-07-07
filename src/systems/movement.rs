58d7b5e8e0c42f15171ba4a0e864a439881b5ef3
Trevor Makes <the.trevor.makes@gmail.com>
2022-07-06 22:59:44 -0700

    Transform movement from for_each to default system

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -1,20 +1,28 @@
 use crate::prelude::*;
 
-#[system(for_each)]
+#[system]
+#[read_component(WantsToMove)]
 #[read_component(Player)]
 pub fn movement(
-    entity: &Entity,
-    want_move: &WantsToMove,
     #[resource] map: &Map,
     #[resource] camera: &mut Camera,
-    ecs: &mut SubWorld,
+    ecs: &SubWorld,
     commands: &mut CommandBuffer,
 ) {
-    if map.can_enter_tile(want_move.destination) {
-        commands.add_component(want_move.entity, want_move.destination);
-        if ecs.entry_ref(want_move.entity).unwrap().get_component::<Player>().is_ok() {
-            camera.on_player_move(want_move.destination);
-        }
-    }
-    commands.remove(*entity);
+    let player_entity = <Entity>::query()
+        .filter(component::<Player>())
+        .iter(ecs)
+        .next().unwrap();
+
+    <(Entity, &WantsToMove)>::query()
+        .iter(ecs)
+        .for_each(|(entity, want_move)| {
+            if map.can_enter_tile(want_move.destination) {
+                commands.add_component(want_move.entity, want_move.destination);
+                if want_move.entity == *player_entity {
+                    camera.on_player_move(want_move.destination);
+                }
+            }
+            commands.remove(*entity);
+        });
 }
